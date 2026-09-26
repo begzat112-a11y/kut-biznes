edc48be7651744505c80899d4d49f58832efe2af
begzat112-a11y <begzat112@gmail.com>
2026-09-26 05:18:59 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 /* =========================================================
-   КУТ: БИЗНЕС — Service Worker v4.2.0
+   КУТ: БИЗНЕС — Service Worker v4.3.0
    HTML — всегда из сети. Кэш только для CSS/иконок.
    ========================================================= */
 
-const CACHE_NAME = 'kut-biznes-v4.2.0';
+const CACHE_NAME = 'kut-biznes-v4.3.0';
 
 const ASSETS = [
   './css/style.css',
