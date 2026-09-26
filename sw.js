11b1791cc943078b0c5b80affb5ed2e228b1bed1
begzat112-a11y <begzat112@gmail.com>
2026-09-26 20:20:09 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    HTML — всегда из сети. Кэш только для CSS/иконок.
    ========================================================= */
 
-const CACHE_NAME = 'kut-biznes-v6.2.1';
+const CACHE_NAME = 'kut-biznes-v6.2.2';
 
 const ASSETS = [
   './css/style.css',
